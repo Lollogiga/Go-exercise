5e6f16b1dd589ba22435904d832541bc38644f5d
LorenzoGrande <lorenzo.grande@hotmail.it>
2024-01-25 12:38:54 +0100

    Progetto Finito--> Da pulire

---
+++ Client/main.go
@@ -0,0 +1,37 @@
+// Package main implements a client for Greeter service.
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	pb "google.golang.org/grpc/examples/Go-Exercise/Protobuf"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+)
+
+func main() {
+	flag.Parse()
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewTimeClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.GetTime(ctx, &pb.TimeRequest{})
+	if err != nil {
+		log.Fatalf("Error on request: %v", err)
+	}
+	log.Printf("	TIME: %s", r.GetMessage())
+}
